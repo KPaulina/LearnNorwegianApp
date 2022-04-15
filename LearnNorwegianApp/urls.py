a10eab657fa260aeb4f117c623d7f22dcac547da
KPaulina <41211513+KPaulina@users.noreply.github.com>
2022-04-15 15:32:32 +0000

    Adding functionality to practice norwegian verbs

--- LearnNorwegianApp/urls.py
+++ LearnNorwegianApp/urls.py
@@ -1,11 +1,24 @@
-from django.urls import path
+"""LearnNorwegianApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 from . import views
-from .views import vocab_to_learn
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('learn', views.learn, name="learn"),
-    path('words', views.vocab_to_learn, name="vocabulary"),
-    path('verbs', views.verbs_to_learn, name="vocabulary"),
-    path('train', views.create_list_of_words, name="vocabulary")
-]
+    path("", views.index),
+    path('admin/', admin.site.urls),
+    path('vocab/', include("vocab.urls"))
+    ]
