3292812ed5931aa3b00521d2d974a9a97fe6eb0a
KPaulina <41211513+KPaulina@users.noreply.github.com>
2022-07-09 11:11:52 +0200

    working on authentication form

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 
 def login_view(requests):
-    if requests.user.is_authenticated:
-        return render(requests, "accounts/already-logged-in.html", {})
     if requests == "POST":
-        username = requests.POST.get("username")
-        password = requests.POST.get("passowrd")
-        user = authenticate(username=username, password=password)
-        if user is None:
-            context = {'error': 'Niepoprawne hasło lub login'}
-            return render(requests, "accounts/login.html", context)
-        login(requests, user)
-        return redirect('/')
-    return render(requests, "accounts/login.html", {})
+        form = AuthenticationForm(requests, data=requests.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(requests, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(requests)
+    context = {'form': form}
+    return render(requests, "accounts/login.html", context)
 
 
 def logout_view(requests):
